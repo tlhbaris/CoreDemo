b342002fb5f612d9fc29960ce45c3407305c59f0
tlhbaris <103104761+tlhbaris@users.noreply.github.com>
2022-11-02 03:25:37 +0300

    SubscribeMail Component

--- EntityLayer/Concrete/NewsLetter.cs
+++ EntityLayer/Concrete/NewsLetter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace EntityLayer.Concrete
 {
     public class NewsLetter
     {
+        [Key]
+        public int MailID { get; set; }
+        public string Mail { get; set; }
+        public bool MailStatus { get; set; }
     }
 }
